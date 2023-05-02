86109e36dd41347d0d4b604ff4511f1ac712ccd9
Arison Reis <arisonreis157@gmail.com>
2023-05-01 21:28:04 -0300

    ✨ feat(createUser.ts): adding user creation

--- src/functions/createUser.ts
+++ src/functions/createUser.ts
@@ -1,16 +1,32 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { IUserDTO } from '../@types/userDTO';
+import { db } from '../database/database';
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const { name, username } = JSON.parse(event.body!) as IUserDTO;
-  console.log(name, username);
 
+  if (!name && !username) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        status: 'error',
+        message: 'no tada received',
+      }),
+    };
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      name: name,
+      username: username,
+    },
+  });
   return {
     statusCode: 201,
     body: JSON.stringify({
-      status: 'ok',
+      newUser,
     }),
   };
 };
